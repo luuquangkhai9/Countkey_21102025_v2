ba79dc9ba450c371248920e717f302c5ad5e8976
Lưu Quang Khải <127973462+luuquangkhai9@users.noreply.github.com>
2025-10-24 17:32:38 +0700

    first commit

---
+++ download_vncore.py
@@ -0,0 +1,4 @@
+import py_vncorenlp
+
+# Tải mô hình về thư mục cụ thể (ví dụ: C:\vncorenlp)
+py_vncorenlp.download_model(save_dir=r"C:\Users\Administrator\Downloads\Telegram Desktop\Countkey_21102025_v2\vncorenlp")
